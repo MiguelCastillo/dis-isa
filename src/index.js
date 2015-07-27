a55fd01c0e5af74224e88959543d2e994275ba97
Miguel Castillo <manchagnu@gmail.com>
2015-07-26 21:02:25 -0400

    initial check in

---
+++ src/index.js
@@ -0,0 +1,154 @@
+// Helper method to test for object types.
+var toString = (function() {
+  var _toString = Object.prototype.toString;
+  return function toStringDelegate(item) {
+    return _toString.call(item);
+  };
+})();
+
+var regexSignature  = toString(/test/);
+var dateSignature   = toString(new Date());
+var arraySignature  = toString([]);
+var objectSignature = toString({});
+var errorSignature  = toString(new Error());
+
+
+/**
+ * Check if input is undefined
+ *
+ * @param {*} item - Item to be tested for undefined
+ * @returns {boolean}
+ */
+function isUndefined(item) {
+  return item === (void 0);
+}
+
+/**
+ * Check if input is null
+ *
+ * @param {*} item - Item to be tested for null
+ * @returns {boolean}
+ */
+function isNull(item) {
+  return item === null;
+}
+
+/**
+ * Check if input is a regulat expression
+ *
+ * @param {*} item - Item to check for regular expression
+ * @returns {boolean}
+ */
+function isRegex(item) {
+  return !!item && toString(item) === regexSignature;
+}
+
+/**
+ * Check if input is an array
+ *
+ * @param {*} item - Item to be tested for Array
+ * @returns {boolean}
+ */
+var isArray = (function() {
+  if (Array.isArray) {
+    return Array.isArray;
+  }
+
+  return function(item) {
+    return toString(item) === arraySignature;
+  };
+})();
+
+/**
+ * Check if input is a function
+ *
+ * @param {*} item - Item to be tested for function
+ * @returns {boolean}
+ */
+function isFunction(item) {
+  return typeof item === 'function';
+}
+
+/**
+ * Check if input is a string
+ *
+ * @param {*} item - Item to check for string
+ * @returns {boolean}
+ */
+function isString(item) {
+  return typeof item === 'string';
+}
+
+/**
+ * Check if input is an object. Objects are:
+ *  - literal object, objects, arrays, true, false, null, and instances
+ *
+ * @param {*} item - Item to check for object
+ * @returns {boolean}
+ */
+function isObject(item) {
+  return typeof item === 'object';
+}
+
+/**
+ * Check if input is a Date
+ *
+ * @param {*} item - Item to be tested for Date
+ * @returns {boolean}
+ */
+function isDate(item) {
+  return toString(item) === dateSignature;
+}
+
+/**
+ * Check if item is an object literal - plain object.
+ *
+ * @param {*} item - Item to check for object literal
+ * @returns {boolean}
+ */
+function isPlainObject(item) {
+  return toString(item) === objectSignature;
+}
+
+/**
+ * Check if input is an error
+ *
+ * @param {*} item - Item to check for error
+ * @returns {boolean}
+ */
+function isError(item) {
+  return toString(item) === errorSignature || item instanceof Error;
+}
+
+/**
+ * Extract the type name. This uses Object.prototype.toString
+ * to get the type name.
+ *
+ * @param {*} item - Item to get the type for
+ * @returns {string} type of the object
+ */
+function typeName(item) {
+  if (isNull(item)) {
+    return 'null';
+  }
+  else if (isUndefined(item)) {
+    return 'undefined';
+  }
+
+  return /\[.+ ([^\]]+)/.exec(toString(item))[1].toLowerCase();
+}
+
+module.exports = {
+  isNull: isNull,
+  isUndefined: isUndefined,
+  isRegex: isRegex,
+  isArray: isArray,
+  isError: isError,
+  isString: isString,
+  isObject: isObject,
+  isPlainObject: isPlainObject,
+  isFunction: isFunction,
+  isDate: isDate,
+  typeName: typeName,
+  toString: toString
+};
---
+++ test/spec/index.js
@@ -0,0 +1,220 @@
+define(["dist/index"], function(types) {
+
+  describe("types Suite", function() {
+    describe("Getting object type name", function() {
+      describe("for object", function() {
+        it("it's object", function() {
+          expect(types.typeName({})).to.equal("object");
+        });
+      });
+
+      describe("for object with no prototype", function() {
+        it("it's object", function() {
+          expect(types.typeName(Object.create(null))).to.equal("object");
+        });
+      });
+
+      describe("for a regex", function() {
+        it("it's regexp", function() {
+          expect(types.typeName(/test/)).to.equal("regexp");
+        });
+      });
+
+      describe("for string", function() {
+        it("it's string", function() {
+          expect(types.typeName("{}")).to.equal("string");
+        });
+      });
+
+      describe("for null", function() {
+        it("it's null", function() {
+          expect(types.typeName(null)).to.equal("null");
+        });
+      });
+
+      describe("for undefined", function() {
+        it("it's undefined", function() {
+          expect(types.typeName(undefined)).to.equal("undefined");
+        });
+      });
+
+      describe("for function", function() {
+        it("it's function", function() {
+          expect(types.typeName(function() {})).to.equal("function");
+        });
+      });
+
+      describe("for true", function() {
+        it("it's boolean", function() {
+          expect(types.typeName(true)).to.equal("boolean");
+        });
+      });
+
+      describe("for false", function() {
+        it("it's boolean", function() {
+          expect(types.typeName(true)).to.equal("boolean");
+        });
+      });
+
+      describe("for 1", function() {
+        it("it's number", function() {
+          expect(types.typeName(1)).to.equal("number");
+        });
+      });
+
+      describe("for 1.3", function() {
+        it("it's number", function() {
+          expect(types.typeName(1.3)).to.equal("number");
+        });
+      });
+    });
+
+    describe("when isPlainObject", function() {
+      describe("empty object", function() {
+        it("is true", function() {
+          expect(types.isPlainObject({})).to.equal(true);
+        });
+      });
+
+      describe("object with no prototype", function() {
+        it("is true", function() {
+          expect(types.isPlainObject(Object.create(null))).to.equal(true);
+        });
+      });
+
+      describe("object with prototype", function() {
+        it("is true", function() {
+          expect(types.isPlainObject(Object.create({}))).to.equal(true);
+        });
+      });
+
+      describe("false", function() {
+        it("is false", function() {
+          expect(types.isPlainObject(false)).to.equal(false);
+        });
+      });
+
+      describe("true", function() {
+        it("is false", function() {
+          expect(types.isPlainObject(true)).to.equal(false);
+        });
+      });
+
+      describe("null", function() {
+        it("is false", function() {
+          expect(types.isPlainObject(null)).to.equal(false);
+        });
+      });
+
+      describe("new Date", function() {
+        it("is false", function() {
+          expect(types.isPlainObject(new Date())).to.equal(false);
+        });
+      });
+
+      describe("undefined", function() {
+        it("is false", function() {
+          expect(types.isPlainObject(undefined)).to.equal(false);
+        });
+      });
+
+      describe("anonymous function", function() {
+        it("is false", function() {
+          expect(types.isPlainObject(function(){})).to.equal(false);
+        });
+      });
+    });
+
+
+    describe("when isObject", function() {
+      describe("empty object", function() {
+        it("is true", function() {
+          expect(types.isObject({})).to.equal(true);
+        });
+      });
+
+      describe("null", function() {
+        it("is true", function() {
+          expect(types.isObject(null)).to.equal(true);
+        });
+      });
+
+      describe("new Date", function() {
+        it("is true", function() {
+          expect(types.isObject(new Date())).to.equal(true);
+        });
+      });
+
+      describe("array", function() {
+        it("is true", function() {
+          expect(types.isObject([])).to.equal(true);
+        });
+      });
+
+      describe("true", function() {
+        it("is true", function() {
+          expect(types.isObject(true));
+        });
+      });
+
+      describe("false", function() {
+        it("is false", function() {
+          expect(types.isObject(true));
+        });
+      });
+
+      describe("instance", function() {
+        it("is true", function() {
+          function test(){}
+          expect(types.isObject(new test())).to.equal(true);
+        });
+      });
+
+      describe("undefined", function() {
+        it("is false", function() {
+          expect(types.isObject(undefined)).to.equal(false);
+        });
+      });
+
+      describe("anonymous function", function() {
+        it("is false", function() {
+          expect(types.isObject(function(){})).to.equal(false);
+        });
+      });
+    });
+
+
+    describe("when isFunction", function() {
+      describe("empty object", function() {
+        it("is false", function() {
+          expect(types.isFunction({})).to.equal(false);
+        });
+      });
+
+      describe("null", function() {
+        it("is false", function() {
+          expect(types.isFunction(null)).to.equal(false);
+        });
+      });
+
+      describe("new Date", function() {
+        it("is false", function() {
+          expect(types.isFunction(new Date())).to.equal(false);
+        });
+      });
+
+      describe("undefined", function() {
+        it("is false", function() {
+          expect(types.isFunction(undefined)).to.equal(false);
+        });
+      });
+
+      describe("anonymous function", function() {
+        it("is true", function() {
+          expect(types.isFunction(function(){})).to.equal(true);
+        });
+      });
+    });
+  });
+
+});
